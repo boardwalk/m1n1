fa4917e8191f696ded4028d844a7d85119979229
Dan Skorupski <boardwalk@ersatsz.com>
2021-11-04 17:33:50 -0500

    Fix warnings

--- src/pcie.c
+++ src/pcie.c
@@ -191,7 +191,7 @@ int pcie_init(void)
         return -1;
     }
 
-    int port;
+    unsigned int port;
     for (port = 0; port < port_count; port++) {
         char bridge[64];
 
@@ -261,7 +261,7 @@ int pcie_shutdown(void)
     if (!pcie_initialized)
         return 0;
 
-    for (int port = 0; port < port_count; port++) {
+    for (unsigned int port = 0; port < port_count; port++) {
         clear32(port_base[port] + APCIE_PORT_RESET, APCIE_PORT_RESET_DIS);
         clear32(port_base[port] + APCIE_PORT_APPCLK, APCIE_PORT_APPCLK_EN);
     }
